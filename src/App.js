12edefa95d5f42d0bb743aa267c09b065084ff69
laujuvi <laujuvi@hotmail.com>
2020-11-13 19:38:02 -0300

    Creado fonts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          src/App.js and save to reload.
         </p>
         <a
           className="App-link"
